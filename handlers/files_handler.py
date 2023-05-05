58adb121b5e59c364c5a835f711e0198283d4064
sergey <spawlov@gmail.com>
2023-05-05 18:28:38 +0700

    Changed files_handler.py - rewrote functions

--- handlers/files_handler.py
+++ handlers/files_handler.py
@@ -1,40 +1,45 @@
 import os
 import random
 
+from handlers.redis_handler import set_question_info
+
 from redis.client import Redis
 
 
-def from_file_to_dict(path: str) -> dict:
-    with open(path, 'r', encoding='KOI8-R') as file:
-        questions = file.readlines()
+def get_dict_from_files(path: str, encode: str) -> dict:
     result = dict()
-    count = 0
-    for count, question in enumerate(questions):
-        if question.startswith('Вопрос '):
-            question_index = count + 1
-            question = answer = ''
-            while question_index < len(questions) - 1 \
-                    and not questions[question_index].startswith('Ответ'):
-                question += questions[question_index].replace('\n', ' ')
-                question_index += 1
-            answer_index = question_index + 1
-            while answer_index < len(questions) - 1 \
-                    and not questions[answer_index].startswith('Вопрос '):
-                answer += questions[answer_index]
-                answer_index += 1
-            result[question.rstrip()] = answer.rstrip()
+    list_files = [_ for _ in os.listdir(path) if _.endswith('.txt')]
+    for filename in list_files:
+        with open(path + filename, 'r', encoding=encode) as file:
+            content = file.readlines()
+        for count, line in enumerate(content):
+            if line.startswith('Вопрос '):
+                question = answer = str()
+                question_line = count + 1
+                while all([
+                    question_line < len(content),
+                    not content[question_line].startswith('Ответ')
+                ]):
+                    question += content[question_line].strip()
+                    question_line += 1
+                answer_line = question_line + 1
+                while all([
+                    answer_line < len(content) - 1,
+                    not content[answer_line].startswith('Вопрос ')
+                ]):
+                    answer += content[answer_line].strip()
+                    answer_line += 1
+                result[question] = {'filename': filename, 'answer': answer}
     return result
 
 
-def get_next_question(rd: Redis, user_id: [int, str]) -> str:
-    filename = random.choice(os.listdir('questions/'))
-    questions = from_file_to_dict(f'questions/{filename}')
+def get_random_question(rd: Redis, user: [int, str], questions: dict) -> str:
     question = random.choice(list(questions.keys()))
-    data = {
-        'filename': filename,
-        'question': question,
-        'answer': questions[question],
-    }
-    rd.delete(user_id)
-    rd.hset(user_id, mapping=data)
+    set_question_info(
+        rd,
+        user,
+        questions[question]['filename'],
+        question,
+        questions[question]['answer']
+    )
     return question
