502816e89f8319fd19444f1eb0836715f5f86f74
sergey <spawlov@gmail.com>
2023-05-05 20:57:28 +0700

    Changed redis_handler.py, removed annotates

--- handlers/redis_handler.py
+++ handlers/redis_handler.py
@@ -1,23 +1,10 @@
-from redis.asyncio.client import Redis
-
-
-def set_question_info(
-        rd: Redis,
-        user: [int, str],
-        filename: str,
-        question: str,
-        answer: str
-) -> any:
-    data = {
-        'file': filename,
-        'question': question,
-        'answer': answer,
-    }
+def set_question_info(rd, user, filename, question, answer):
+    data = {'file': filename, 'question': question, 'answer': answer}
     rd.delete(user)
     return rd.hset(user, mapping=data)
 
 
-def get_question_info(rd: Redis, user: [int, str]) -> tuple:
+def get_question_info(rd, user):
     redis_data = rd.hgetall(user)
     return (
         redis_data[b'file'].decode('utf-8'),
