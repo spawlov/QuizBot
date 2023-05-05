b7a67d79520769acea8746cf526b0d731d4b3355
sergey <spawlov@gmail.com>
2023-05-05 19:00:55 +0700

    Changed redis_handler.py: added get_question_info function

--- handlers/redis_handler.py
+++ handlers/redis_handler.py
@@ -15,3 +15,12 @@ def set_question_info(
     }
     rd.delete(user)
     return rd.hset(user, mapping=data)
+
+
+def get_question_info(rd: Redis, user: [int, str]) -> tuple:
+    redis_data = rd.hgetall(user)
+    return (
+        redis_data[b'file'].decode('utf-8'),
+        redis_data[b'question'].decode('utf-8'),
+        redis_data[b'answer'].decode('utf-8')
+    )
